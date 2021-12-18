3f8fd883b1cec9bc29206db99bf3b6a80d5aab7f
mano541 <katkurimanoj47@gmail.com>
2021-12-18 15:35:24 +0530

    secound message

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 provider "aws" {
-	region = "ap-south-1"
+  region = "ap-south-1"
 }
+
+resource "aws_instance" "web" {
+  ami           = var.image
+  instance_type = var.instancetype
+  count         = 2
+
+  tags = {
+    name = "web server"
+  }
+}
+
+
+
+
+
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "instancetype" {
+  #default = "t2.micro"
+}
+variable "image" {
+  #default = "ami-052cef05d01020f1d"
+}
